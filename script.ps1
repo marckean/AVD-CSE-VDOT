8a99ce956c2fe40a44cffb566adf594343dab809
Marc Kean <marc@marckean.com>
2023-07-19 18:13:50 +1000

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,4 @@
-##############################################################
-#  Local Security Group
-##############################################################
-net localgroup /add "AVD Users"
+$repo = "raw.githubusercontent.com/marckean/AVD-CSE-VDOT/main"
 
 ##############################################################
 #  AppLocker configuration
@@ -9,11 +6,45 @@ net localgroup /add "AVD Users"
 # Block %SYSTEM32%\Taskmgr.exe for BUILTIN\Users
 $path = "$env:TEMP\AppLocker"
 $policyName = "AppLockerPolicy.xml"
-$policyPath = "https://raw.githubusercontent.com/marckean/AVD-CSE-VDOT/main/$policyName"
+$policyPath = "https://$repo/$policyName"
 
-if(!(Test-Path "$env:TEMP\AppLocker")){New-Item -Path $path -ItemType Directory}
+Remove-Item "$path\$policyName" -Force -ErrorAction SilentlyContinue
+if(!(Test-Path $path)){New-Item -Path $path -ItemType Directory}
 Start-BitsTransfer -Source $policyPath -Destination $path
 Set-AppLockerPolicy -XMLPolicy "$path\$policyName"
+
+##############################################################
+#  LGPO.exe - Import and apply policy settings
+##############################################################
+$path = "$env:SystemRoot\System32"
+Start-BitsTransfer -Source "https://$repo/LGPO.exe" -Destination $path
+
+##############################################################
+#  LGPO.exe - Import and apply policy settings
+##############################################################
+$zip = "https://github.com/marckean/AVD-CSE-VDOT/raw/main/SignalAVD_LGPO.zip"
+$localZip = "$env:TEMP\SignalAVD_LGPO.zip"
+Remove-Item $localZip -Force -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri $zip -OutFile $localZip -ErrorAction 'Stop'
+# Extract VDOT from ZIP archive
+Expand-Archive -Path $localZip -DestinationPath "$env:TEMP\SignalAVD_LGPO" -Force
+LGPO.exe /g "$env:TEMP\SignalAVD_LGPO"
+
+##############################################################
+#  Install Signal
+##############################################################
+$path = "$env:TEMP\SignalInstall"
+$exeName = "signal-desktop-win-6.25.0.exe"
+$exePath = "https://updates.signal.org/desktop/$exeName"
+
+Remove-Item "$path\$exeName" -Force -ErrorAction SilentlyContinue
+if(!(Test-Path $path)){New-Item -Path $path -ItemType Directory}
+Start-BitsTransfer -Source $exePath -Destination $path
+
+$Command = "$path\$exeName /S"
+Invoke-Expression $Command -Verbose
+Start-Sleep -Seconds 60
+Copy-Item -Path "$env:USERPROFILE\AppData\Local\Programs\signal-desktop\*" -Destination "$env:ProgramFiles\signal-desktop" -Recurse -Force
 
 ##############################################################
 #  Set the AppLocker service to auto
